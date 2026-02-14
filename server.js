14c9cafbc7e802ab67beadd4c4f56979fe4915eb
Subrat Hota <msubha053@gmail.com>
2026-02-14 17:32:22 +0530

    cors added

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.set("connectedUsers", connectedUsers);
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://coin-minning.onrender.com"], // Add your frontend origins here
+    origin: ["http://localhost:5173", "https://coin-minning.onrender.com", "https://olaroclub.online"], // Add your frontend origins here
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
